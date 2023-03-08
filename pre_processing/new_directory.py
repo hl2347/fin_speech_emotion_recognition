e1b1dab519e7b41d0b9d224f8252cba7568f3d88
hl2347 <hl2347@nyu.edu>
2023-03-08 16:03:50 +0400

    a code to save wav file into csv files, file lengths are adjusted to 2.5 sec(55125)

--- pre_processing/new_directory.py
+++ pre_processing/new_directory.py
@@ -23,6 +23,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization,Activation
 # from keras.utils import np_utils, to_categorical
 from keras.callbacks import ModelCheckpoint
+from pre_processing import *
 
 import warnings
 if not sys.warnoptions:
@@ -31,149 +32,43 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 #Data path
-Ravdess = "input/ravdess-emotional-speech-audio/audio_speech_actors_01-24/"
-Crema = "input/cremad/AudioWAV/"
-Tess = "input/Tess/Tess/"
-Savee = "input/surrey-audiovisual-expressed-emotion-savee/ALL/"
+data_path = pd.read_csv("data_path.csv") #columns: Emotions, Path, dataset
 
-#Load Dataset
-ravdess_directory_list = os.listdir(Ravdess)
+#For each dataset
+dataset_names = ["ravdess","savee","tess","crema"]
+dataset_list = []
+counter = [[0 for i in range(8)] for x in range(3)]
+for name in dataset_names:
+    df = pd.DataFrame(data_path.loc[data_path["dataset"] == name])
+    #8 emotions
+    emotion_list = ['disgust', 'neutral', 'fear', 'calm', 'happy', 'angry', 'surprise', 'sad']
+    for emotion_index, emotion in enumerate(emotion_list):
+        emotion_df = df.loc[df.Emotions == emotion]
+        emotion_df = emotion_df.sample(frac=1,random_state = 0)
+        train_index = int(len(emotion_df) * 0.7)
+        validation_index = train_index+int(len(emotion_df)*0.15)
 
-file_emotion = []
-file_path = []
-for dir in ravdess_directory_list:
-    # as their are 20 different actors in our previous directory we need to extract files for each actor.
-    actor = os.listdir(Ravdess + dir)
-    for file in actor:
-        part = file.split('.')[0]
-        part = part.split('-')
-        # third part in each file represents the emotion associated to that file.
-        file_emotion.append(int(part[2]))
-        file_path.append(Ravdess + dir + '/' + file)
+        train_df = emotion_df.iloc[:train_index,:]
+        val_df = emotion_df.iloc[train_index:validation_index,:]
+        test_df = emotion_df.iloc[validation_index:,:]
         
-# dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+        for i in range(len(train_df)):
+            data,sr = librosa.load(train_df.iloc[i,1],duration=2.5, offset=0.6)
+            data = adjust_length(data,2.5)
+            data = pd.DataFrame(data)
+            data.to_csv("fixed_new_directory/train/"+emotion+"_"+str(counter[0][emotion_index])+".csv",index=False)
+            counter[0][emotion_index] += 1
 
-# dataframe for path of files.
-path_df = pd.DataFrame(file_path, columns=['Path'])
-Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
+        for i in range(len(val_df)):
+            data,sr = librosa.load(val_df.iloc[i,1],duration=2.5, offset=0.6)
+            data = adjust_length(data,2.5)
+            data = pd.DataFrame(data)
+            data.to_csv("fixed_new_directory/validation/"+emotion+"_"+str(counter[1][emotion_index])+".csv",index=False)
+            counter[1][emotion_index] += 1
 
-# changing integers to actual emotions.
-Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
-
-crema_directory_list = os.listdir(Crema)
-
-file_emotion = []
-file_path = []
-
-for file in crema_directory_list:
-    # storing file paths
-    file_path.append(Crema + file)
-    # storing file emotions
-    part=file.split('_')
-    if part[2] == 'SAD':
-        file_emotion.append('sad')
-    elif part[2] == 'ANG':
-        file_emotion.append('angry')
-    elif part[2] == 'DIS':
-        file_emotion.append('disgust')
-    elif part[2] == 'FEA':
-        file_emotion.append('fear')
-    elif part[2] == 'HAP':
-        file_emotion.append('happy')
-    elif part[2] == 'NEU':
-        file_emotion.append('neutral')
-    else:
-        file_emotion.append('Unknown')
-        
-# dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
-
-# dataframe for path of files.
-path_df = pd.DataFrame(file_path, columns=['Path'])
-Crema_df = pd.concat([emotion_df, path_df], axis=1)
-
-tess_directory_list = os.listdir(Tess)
-
-file_emotion = []
-file_path = []
-
-for dir in tess_directory_list:
-    directories = os.listdir(Tess + dir)
-    for file in directories:
-        part = file.split('.')[0]
-        part = part.split('_')[2]
-        if part=='ps':
-            file_emotion.append('surprise')
-        else:
-            file_emotion.append(part)
-        file_path.append(Tess + dir + '/' + file)
-        
-# dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
-
-# dataframe for path of files.
-path_df = pd.DataFrame(file_path, columns=['Path'])
-Tess_df = pd.concat([emotion_df, path_df], axis=1)
-
-savee_directory_list = os.listdir(Savee)
-
-file_emotion = []
-file_path = []
-
-for file in savee_directory_list:
-    file_path.append(Savee + file)
-    part = file.split('_')[1]
-    ele = part[:-6]
-    if ele=='a':
-        file_emotion.append('angry')
-    elif ele=='d':
-        file_emotion.append('disgust')
-    elif ele=='f':
-        file_emotion.append('fear')
-    elif ele=='h':
-        file_emotion.append('happy')
-    elif ele=='n':
-        file_emotion.append('neutral')
-    elif ele=='sa':
-        file_emotion.append('sad')
-    else:
-        file_emotion.append('surprise')
-        
-# dataframe for emotion of files
-emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
-
-# dataframe for path of files.
-path_df = pd.DataFrame(file_path, columns=['Path'])
-Savee_df = pd.concat([emotion_df, path_df], axis=1)
-
-data_path = pd.concat([Ravdess_df, Crema_df, Tess_df, Savee_df], axis = 0)
-print("loaded data")
-
-
-#8 emotions
-emotion_list = ['disgust', 'neutral', 'fear', 'calm', 'happy', 'angry', 'surprise', 'sad']
-for emotion in emotion_list:
-    emotion_df = data_path.loc[data_path.Emotions == emotion]
-    emotion_df = emotion_df.sample(frac=1)
-    train_index = int(len(emotion_df) * 0.7)
-    validation_index = train_index+int(len(emotion_df)*0.15)
-
-    train_df = emotion_df.iloc[:train_index,:]
-    val_df = emotion_df.iloc[train_index:validation_index,:]
-    test_df = emotion_df.iloc[validation_index:,:]
-    
-    for i in range(len(train_df)):
-        data = librosa.load(train_df.iloc[i,1],duration=2.5, offset=0.6)[0].T
-        data = pd.DataFrame(data)
-        data.to_csv("new_directory/train/"+emotion+"_"+str(i)+".csv",index=False)
-
-    for i in range(len(val_df)):
-        data = librosa.load(val_df.iloc[i,1],duration=2.5, offset=0.6)[0].T
-        data = pd.DataFrame(data)
-        data.to_csv("new_directory/validation/"+emotion+"_"+str(i)+".csv",index=False)
-
-    for i in range(len(test_df)):
-        data = librosa.load(test_df.iloc[i,1],duration=2.5, offset=0.6)[0].T
-        data = pd.DataFrame(data)
-        data.to_csv("new_directory/test/"+emotion+"_"+str(i)+".csv",index=False)
+        for i in range(len(test_df)):
+            data,sr = librosa.load(test_df.iloc[i,1],duration=2.5, offset=0.6)
+            data = adjust_length(data,2.5)
+            data = pd.DataFrame(data)
+            data.to_csv("fixed_new_directory/test/"+emotion+"_"+str(counter[2][emotion_index])+".csv",index=False)
+            counter[2][emotion_index] += 1
